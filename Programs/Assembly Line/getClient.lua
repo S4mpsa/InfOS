8336d49c9562f3e641d26b0613155cf9eba6f15c
Sampsa <sampo.vanninen@aalto.fi>
2020-11-11 01:04:04 +0200

    Add script to download files from Github

---
+++ Programs/Assembly Line/getClient.lua
@@ -0,0 +1,17 @@
+local path = "/home"
+local shell = require("shell")
+local download_list = 
+{
+    "https://raw.githubusercontent.com/S4mpsa/InfOS/master/Programs/Assembly%20Line/assemblyClient.lua",
+    "https://raw.githubusercontent.com/S4mpsa/InfOS/master/Programs/Assembly%20Line/dictionary.lua",
+    "https://raw.githubusercontent.com/S4mpsa/InfOS/master/Programs/Assembly%20Line/transport.lua",
+    "https://raw.githubusercontent.com/S4mpsa/InfOS/master/Programs/Assembly%20Line/util.lua",
+}
+shell.setWorkingDirectory(path)
+print("Updating Files")
+for k,v in pairs(download_list) do
+    print("Fetching ",v)
+    local command = "wget "..v.." -f"
+    shell.execute(command)
+end
+shell.setWorkingDirectory("/home")
