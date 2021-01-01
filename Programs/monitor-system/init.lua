dc26aece93f65aa97f8166fc37e15ac7c8848caa
Gabriel Moreira Minossi <gordominossi@gmail.com>
2021-01-01 19:13:38 -0300

    Adding getStatusUsecase

--- Programs/monitor-system/init.lua
+++ Programs/monitor-system/init.lua
@@ -15,25 +15,34 @@ local getEnergyStatus = require("domain.energy.get-energy-status-usecase")
 --
 
 local cleanroom = MultiBlock:new(multiBlockAddresses.cleanroom)
+cleanroom.name = "cleanroom"
 local cleanroomMachines = {}
 for _, address in pairs(cleanroomAddresses) do
     table.insert(cleanroomMachines, SingleBlock:new(address))
 end
 
 local EBF11 = MultiBlock:new(multiBlockAddresses.EBF11)
+EBF11.name = "EBF11"
 
 local multiblocks = {cleanroom, EBF11}
 
 local energyBuffer = EnergyProvider:new(energyBufferAddress.batteryBuffer1)
 
 local energyProducers = {}
+local multiblockStatuses = {}
 
 for i = 0, 100 do
     print(i)
     protectCleanroomRecipes(cleanroom, cleanroomMachines)
-    -- local multiblockStatuses = getMultiblockStatuses(multiblocks)
+    multiblockStatuses = getMultiblockStatuses(multiblocks)
     local energyStatus = getEnergyStatus(energyProducers, energyBuffer)
     os.sleep(0)
     i = i + 1
 end
-require('resources.sound.zelda-secret')()
+
+for multiblock, status in pairs(multiblockStatuses) do
+    print(multiblock .. ": \
+    problems: " .. status.problems .. "\
+    efficiency: " .. status.efficiencyPercentage)
+end
+require("resources.sound.zelda-secret")()
