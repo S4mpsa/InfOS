2fd7bd311ea0c88aa9ae78b4954ab8412180b7d8
Gabriel Moreira Minossi <gordominossi@gmail.com>
2021-01-14 19:29:48 -0300

    Integrating power widget with it's datasource

--- Programs/monitor-system/domain/energy/get-energy-status-usecase.lua
+++ Programs/monitor-system/domain/energy/get-energy-status-usecase.lua
@@ -1,13 +1,14 @@
 -- Import section
-local getConsumption = require("domain.energy.get-consumption-usecase")
-local getProduction = require("domain.energy.get-production-usecase")
+Machine = require("data.datasource.machines")
 --
 
-local function exec(energyBuffer)
+local function exec(address, name)
+    local energyBuffer = Machine.getMachine(address, name)
     -- local comsumption = getConsumption(energyBuffer)
     -- local production = getProduction(energyBuffer)
     local consumption = energyBuffer:getAverageInput()
     local production = energyBuffer:getAverageOutput()
+    local state = {name = "ON", color = Colors.workingColor}
 
     local changeRate = production - consumption
 
@@ -17,14 +18,16 @@ local function exec(energyBuffer)
 
     local energyLimit = changeRate > 0 and maximumEnergy or 0
 
-    local timeToFull = changeRate > 0 and (energyLimit - currentEnergy) / changeRate or nil
-    local timeToEmpty = changeRate < 0 and (energyLimit - currentEnergy) / changeRate or nil
+    local timeToFull = changeRate > 0 and math.floor((energyLimit - currentEnergy) / changeRate) or nil
+    local timeToEmpty = changeRate < 0 and math.floor((energyLimit - currentEnergy) / changeRate) or nil
 
     return {
-        consumption = consumption,
-        production = production,
+        progress = currentEnergy,
+        maxProgress = maximumEnergy,
+        dProgress = changeRate,
         timeToFull = timeToFull,
-        timeToEmpty = timeToEmpty
+        timeToEmpty = timeToEmpty,
+        state = state
     }
 end
 
